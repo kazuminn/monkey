6376b6a2d388e5925e90cc32664b8f161e7c280c
Kinoshita Kazumi <e145702@ie.u-ryukyu.ac.jp>
2019-01-10 22:41:36 +0900

    first commit

---
+++ lexer/lexer_test.go
@@ -0,0 +1,23 @@
+package lexer
+
+import (
+	"testing"
+
+	"monkey/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := "=+(){},;"
+
+	test := []struct{
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS,   "+"},
+		{token.RPAREN, "("},
+		{token.LPAREN, ")"},
+
+
+	}
+}
