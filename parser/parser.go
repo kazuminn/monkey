4c95bf2de64517f164dcd906813a060733e1e460
Kinoshita Kazumi <e145702@ie.u-ryukyu.ac.jp>
2019-01-15 22:46:44 +0900

    構文解析器の第一歩

--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,32 @@
 package parser
+
+import (
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken token.Token
+	preToken token.Token
+}
+
+func New(l *lexer.Lexer) {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
